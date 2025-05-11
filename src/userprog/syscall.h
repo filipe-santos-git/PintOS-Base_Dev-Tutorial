be505135136e375edfef8056ffe52c8d970bfd81
Luiz Gustavo <luizg0509@gmail.com>
2025-05-10 22:57:49 -0300

    Part_2: Modificacoes iniciais

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,37 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-void syscall_init (void);
+void syscall_init(void);
+typedef int pid_t;
 
+#define EXIT_SUCCESS 0 /* Successful execution. */
+#define EXIT_FAILURE 1 /* Unsuccessful execution. */
+
+// Copiado do src/lib/user/syscall.h
+/* Projects 2 and later. */
+void halt(void);
+void exit(int status);
+pid_t exec(const char *file);
+int wait(pid_t);
+bool create(const char *file, unsigned initial_size);
+bool remove(const char *file);
+int open(const char *file);
+int filesize(int fd);
+int read(int fd, void *buffer, unsigned length);
+int write(int fd, const void *buffer, unsigned length);
+void seek(int fd, unsigned position);
+unsigned tell(int fd);
+void close(int fd);
+
+/* Project 3 and optionally project 4. */
+// mapid_t mmap (int fd, void *addr);
+// void munmap (mapid_t);
+//
+// /* Project 4 only. */
+// bool chdir (const char *dir);
+// bool mkdir (const char *dir);
+// bool readdir (int fd, char name[READDIR_MAX_LEN + 1]);
+// bool isdir (int fd);
+// int inumber (int fd);
+//
 #endif /* userprog/syscall.h */
